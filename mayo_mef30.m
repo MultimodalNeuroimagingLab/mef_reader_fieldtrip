f1a108f13d621e623c342cfb9ed824f21c0383c3
Richard Jie Cui <richard.cui@utoronto.ca>
2020-03-22 00:37:44 -0500

    update mayo_mef30.m

--- mayo_mef30.m
+++ mayo_mef30.m
@@ -3,13 +3,16 @@ function mayo_out = mayo_mef30(varargin)
 %
 % Syntax:
 %   hdr = mayo_mef30(filename)
-%   evt = mayo_mef30(filename, hdr)
-%   dat = mayo_mef30(filename, hdr, begsample, endsample, chanindx)
+%   hdr = mayo_mef30(filename, password)
+%   evt = mayo_mef30(filename, password, hdr)
+%   dat = mayo_mef30(filename, password, hdr, begsample, endsample, chanindx)
 %
 % Input(s):
 %   filename        - [char] name of the file or folder of the dataset
+%   password        - [struct] (opt) password structure of MEF 3.0 data (see
+%                     MEFSession_3p0)
 %   hdr             - [struct] (opt) header structure of the dataset (see
-%                     FT_READ_HEADER; default = struct([]))
+%                     ft_read_header; default = struct([]))
 %   begsample       - [num] (opt) first sample to read (default = [])
 %   endsample       - [num] (opt) last smaple to read (default = [])
 %   chanindx        - [num] (opt) list of channel indices to read (default
@@ -44,6 +47,11 @@ function mayo_out = mayo_mef30(varargin)
 % =========================================================================
 q = parseInputs(varargin{:});
 filename    = q.filename;
+password    = q.password;
+if isempty(password)
+    password = struct('Level1Password', '', 'Level2Password', '',...
+        'AccessLevel', 1);
+end % if
 hdr         = q.hdr;
 begsample   = q.begsample;
 endsample   = q.endsample;
@@ -54,18 +62,20 @@ chanindx    = q.chanindx;
 % =========================================================================
 % setup the instance of the object
 % --------------------------------
-mef_ft = MEFFieldTrip_3p0(filename); % dealing MEF 3.0 data for FieldTrip
+[sesspath, chan_sel] = MEFFieldTrip_3p0.findSessPath(filename);
+mef_ft = MEFFieldTrip_3p0(sesspath); % dealing MEF 3.0 data for FieldTrip
 
 % get the desired information
 % ---------------------------
 switch nargin
-    case 1
+    case {1, 2}
         % get header
-        mayo_out = mef_ft.getHeader;
-    case 2
-        mayo_out = mef_ft.getEvent(hdr);
-    case 5
-        mayo_out = mef_ft.getData(hdr, begsample, endsample, chanindx);
+        mayo_out = mef_ft.getHeader(chan_sel, password);
+    case 3
+        mayo_out = mef_ft.getEvent(chan_sel, password, hdr);
+    case 6
+        mayo_out = mef_ft.getData(chan_sel, password, hdr, begsample,...
+            endsample, chanindx);
     otherwise
         % error
         error('FieldTrip:mayo_mef30:invalidInput',...
@@ -80,6 +90,8 @@ end % function mayo_mef30
 function q = parseInputs(varargin)
 
 % default
+default_pw = struct('Level1Password', '', 'Level2Password', '',...
+    'AccessLevel', 1);
 default_hr = struct([]);
 default_bs = [];
 default_es = [];
@@ -88,6 +100,7 @@ default_ci = [];
 % parse rule
 p = inputParser;
 p.addRequired('filename', @ischar);
+p.addOptional('password', default_pw, @isstruct);
 p.addOptional('hdr', default_hr, @isstruct);
 p.addOptional('begsample', default_bs, @isnumeric);
 p.addOptional('endsample', default_es, @isnumeric);
