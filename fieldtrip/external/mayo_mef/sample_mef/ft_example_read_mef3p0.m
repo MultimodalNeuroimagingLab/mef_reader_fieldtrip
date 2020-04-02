9da19c92671f9a43ce1dc3c459e40b211912100a
Richard Jie Cui <richard.cui@utoronto.ca>
2020-04-02 12:01:09 -0500

    update ft_example_read_mef3p0.m

--- fieldtrip/external/mayo_mef/sample_mef/ft_example_read_mef3p0.m
+++ fieldtrip/external/mayo_mef/sample_mef/ft_example_read_mef3p0.m
@@ -1,6 +1,6 @@
 % FT_EXAMPLE_READ_MEF3P0 an example to read MEF 3.0 data set into FieldTrip
 
-% clear all
+%% clear all
 clearvars
 
 % look for session path of data
@@ -14,9 +14,24 @@ password = struct('Level1Password', 'password1', 'Level2Password',...
 % get the object to deal with FieldTrip
 mef_ft = MEFFieldTrip_3p0(sesspath, password);
 
-% data may be imported with 'mef_ft', but let's demonstrate the interaction
-% with FieldTrip. Let's import 10 seconds data at the beginning of the
-% recording with an assumption that the trigger was at 0 second.
+%% now let's import two channels Left_Occipital-Ref and left-right_occipital
+% for the first 10 seconds
+mef_ft.SelectedChannel = ["Left_Occipital-Ref" "left-right_occipital"];
+mef_ft.StartEnd = [0 10];
+mef_ft.SEUnit = 'Second';
+[X, t] = mef_ft.importSession;
+fs = mef_ft.SamplingFrequency;
+
+figure
+ph = plot(t/fs, X);
+legend(ph, mef_ft.SelectedChannel)
+xlim([0, 1])
+xlabel('Time (s)')
+ylabel('Amplitude')
+
+%% let's demonstrate more interaction with FieldTrip. Let's import 10 seconds
+% data at the beginning of the recording with an assumption that the
+% trigger was at 0 second.
 in_unit = 'second';
 be_second = [0, 10, 0]; % 10-second time of data from the start
 out_unit = 'index';
@@ -43,7 +58,7 @@ brwview = ft_databrowser(cfg, dat_ieeg);
 
 %% Copyright 2020 Richard J. Cui
 % Created: Sun 03/22/2020  9:03:27.318 PM;
-% Revision: 0.1  Date: Sun 03/22/2020  9:03:27.336 PM
+% Revision: 0.2  Date: Thu 04/02/2020 11:30:27.749 AM
 %
 % Multimodel Neuroimaging Lab (Dr. Dora Hermes)
 % Mayo Clinic St. Mary Campus
