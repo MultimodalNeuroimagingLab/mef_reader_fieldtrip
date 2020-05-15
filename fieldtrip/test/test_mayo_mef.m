445800560e73f3a9720d1e855ef2f6b37f9a7b15
Richard Jie Cui <richard.cui@utoronto.ca>
2020-05-15 12:44:00 -0500

    update test_mayo_mef.m

--- fieldtrip/test/test_mayo_mef.m
+++ fieldtrip/test/test_mayo_mef.m
@@ -1,52 +1,70 @@
-% TEST_MAYO_MEF an example to read MEF 3.0/2.1 dataset into FieldTrip
-% 
-% Note:
-%   `test_mayo_mef.m` should be in the folder /root/of/fieldtrip/test.
+function test_mayo_mef
 
-%% Test reading MEF 3.0 dataset
-clear
+% WALLTIME 00:10:00
+% MEM 3gb
+% DEPENDENCY read_mayo_mef21 read_mayo_mef30
 
-% look for session path of data
-p = mfilename('fullpath');
-sesspath = fullfile(fileparts(fileparts(p)), 'external', 'mayo_mef', 'sample_mef',...
-    'mef_3p0.mefd');
+% =========================================================================
+% Note of sample dataset
+% =========================================================================
+% Go to https://github.com/MultimodalNeuroimagingLab/mef_reader_fieldtrip
+% and follow the instructions to download the sample dataset. Set the path
+% to the sample dataset below.
 
-% set the password
-password = struct('Level1Password', 'password1', 'Level2Password',...
+%% set path to sample dataset
+% please set the path to sample dataset
+mef21_data = '/path/to/sample_mef/mef_2p1';
+mef30_data = '/path/to/sample_mef/mef_3p0.mefd';
+
+
+%% install toolbox and build mex files
+disp('-----------------------------------')
+disp('install toolbox and build mex files')
+disp('-----------------------------------')
+ft_hastoolbox('mayo_mef',1);
+make_mayo_mex
+
+%% MEF version 2.1
+disp(' ')
+disp('--------------------------')
+disp('testing MEF version 2.1...')
+disp('--------------------------')
+
+% the password of MEF 2.1 sample dataset 
+mef21_pw = struct('Subject','erlichda','Session','sieve','Data','');
+
+% low-level
+hdr = ft_read_header(mef21_data,'password',mef21_pw);
+dat = ft_read_data(mef21_data,'password',mef21_pw);
+event = ft_read_event(mef21_data,'password',mef21_pw);
+
+% high-level
+cfg = [];
+cfg.dataset = mef21_data;
+cfg.password = mef21_pw;
+data = ft_preprocessing(cfg);
+
+%% MEF version 3.0
+disp(' ')
+disp('--------------------------')
+disp('testing MEF version 3.0...')
+disp('--------------------------')
+
+% the password of MEF 3.0 sample dataset
+mef30_pw = struct('Level1Password', 'password1', 'Level2Password',...
     'password2', 'AccessLevel', 2);
 
+% low-level
+hdr = ft_read_header(mef30_data,'password',mef30_pw);
+dat = ft_read_data(mef30_data,'password',mef30_pw);
+event = ft_read_event(mef30_data,'password',mef30_pw);
 
-%% read data with ft_read_data() but specifying time interval using seconds
-% -------------------------------------------------------------------------
-% Let's import 1.5 seconds data at the beginning of the recording, where
-% the trigger offset is -0.5 second
-hdr = ft_read_header(sesspath, 'password', password);
-fs  = hdr.Fs;
-start_sample = 1;
-end_sample = round(1.5*fs);
-dat = ft_read_data(sesspath,...
-    'begsample', start_sample,...
-    'endsample', end_sample,...
-    'header', hdr,...
-    'password', password,...
-    'chanindx', [4 1 2 3]); % the order of read data can be decided by
-                            % key-value 'chanindx'
+% high-level
+cfg = [];
+cfg.dataset = mef30_data;
+cfg.password = mef30_pw;
+data = ft_preprocessing(cfg);
 
-t = linspace(0, 1.5, end_sample-start_sample+1)-0.5;                            
-figure
-plot(t, dat')
-line(gca, [0 0], ylim, 'Color', 'k')
-xlabel('Time (s)')
-ylabel('Signal amplitude')
-legend(hdr.label{4}, hdr.label{1}, hdr.label{2}, hdr.label{3})
-
-
-%% Copyright (c) 2020 MNL group
-% Created on Fri 04/17/2020 12:21:46.992 PM
-% Revision: 0.1  Date: Fri 04/17/2020 12:21:46.992 PM
-%
-% Multimodal Neuroimaging Lab (Dr. Dora Hermes)
-% Mayo Clinic St. Mary Campus
-% Rochester, MN 55905
-%
-% Email: richard.cui@utoronto.ca (permanent), Cui.Jie@mayo.edu (official)
+disp('********************')
+disp('* You are all set! *')
+disp('********************')
